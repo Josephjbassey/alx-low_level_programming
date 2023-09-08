6422595d20039879ef96e3f78e173e23a7271292
Joseph J Bassey <136816366+Josephjbassey@users.noreply.github.com>
2023-09-08 16:44:53 -0700

    Create a C program to print the lowercase alphabet excluding 'q' and 'e'
    This C program prints the lowercase alphabet, excluding the letters 'q' and 'e,' followed by a new line.
    t accomplishes this task using the putchar function while adhering to the restriction of using putchar only twice, and all code is contained within the main function.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * alpha - a char variable
+ *
+ * This will print all the alphabet on new lines except 'e' and 'q'
+ *
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+			putchar(alpha);
+	}
+	putchar('\n');
+
+	return (0);
+}
