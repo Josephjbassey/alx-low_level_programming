804573517e0896c18df7287a6587784a38e4e038
Joseph J Bassey <136816366+Josephjbassey@users.noreply.github.com>
2023-09-08 17:00:41 -0700

    Create a C program to print all single-digit numbers in base 10
    This C program prints all single-digit numbers in base 10, starting from 0 and going up to 9, each followed by a new line. All code for this program is contained within the main function.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print all single digit numbers of base 10 starting 0
+ *
+ * num - variable name
+ *
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		num++
+	}
+	putchar('\n');
+
+	return (0);
+}
+
