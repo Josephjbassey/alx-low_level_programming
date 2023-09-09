fcb4743d775dc753b4ce4382aac38bc5bf1a5553
Joseph J Bassey <136816366+Josephjbassey@users.noreply.github.com>
2023-09-08 19:10:37 -0700

    Create a C program to print all possible different combinations of two digits
    This C program prints all possible different combinations of two digits in ascending order. The combinations are separated by ",, " and adhere to the specified constraints: it uses only the putchar function, contains all code within the main function, uses putchar only five times at most, and does not use variables of type char.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of two digits
+ * separated by ',' and '' using putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num_1, num_2;
+
+	for (num_1 = 0; num_1 < 10; num_1++)
+	{
+		for (num_2 = 0; num_2 < 10; num_2++)
+		{
+			if (num_1 == num_2 || num_1 > num_2)
+				continue;
+
+			putchar(num_1 + '0');
+			putchar(num_2 + '0');
+
+			if (num_1 == 8 && num_2 == 9)
+				break;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
