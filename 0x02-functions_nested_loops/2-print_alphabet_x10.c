974f8568b53c66c80510778f4a06edbf92856875
Joseph J Bassey <136816366+Josephjbassey@users.noreply.github.com>
2023-09-12 12:24:37 -0700

    Implement a function to print the lowercase alphabet 10 times
    This function is useful for printing the alphabet repeatedly for various purposes.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print the alphabet 10x
+ *
+ * times - variable name for  numuber of iterations
+ *
+ * c - variable name for alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int times;
+	char c;
+
+	for (times = 0; times <= 10; times++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar('c');
+		}
+		_putchar('\n');
+	}
+}
