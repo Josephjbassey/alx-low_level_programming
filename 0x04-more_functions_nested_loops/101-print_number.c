e57240d428b2f8635536271f4c271d4be0b1e06d
Joseph J Bassey <136816366+Josephjbassey@users.noreply.github.com>
2023-09-16 20:17:55 -0700

    Implement the print_number function
    This function prints an integer 'n' using the _putchar function. It does not use long, arrays, pointers, or hard-coded special values.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,19 @@
  */
 void print_number(int n)
 {
+	int num;
+
 	if (n < 0)
 	{
+		num = -n;
 		_putchar('-');
-		n = -n;
 	}
-	if (n / 10 != 0)
+	else
+	{
+		num = n;
+	}
+
+	if (n / 10)
 	{
 		print_number(n / 10);
 	}
