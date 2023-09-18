0ba214be6ba409d9acd5d3138917d3037c38b249
Joseph J Bassey <136816366+Josephjbassey@users.noreply.github.com>
2023-09-18 15:23:12 -0700

    Implement print_array function
    This commit introduces the print_array function, which takes an array of integers and the number of elements to print as parameters. The function prints the specified number of elements from the array, followed by a new line. It provides a convenient way to display a subset of integer values from an array. This function enhances the usability of the codebase by enabling customized array element output.

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: pointer to array
+ * @n: number of array elements.
+ */
+
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+		if (index != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
