95de5d803115230516c2b5d769149f4811d91dfb
Joseph J Bassey <136816366+Josephjbassey@users.noreply.github.com>
2023-09-18 14:04:41 -0700

    Implement puts2 function
    This commit introduces the puts2 function, which prints every other character of a given string, starting with the first character. The function takes a pointer to a string as its argument and iterates through the string, printing characters at even indices. After printing the characters, it adds a new line to the output. This function is useful for displaying a subset of characters from a string.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string, starting with the first
+ * character, followed by a new line.
+ *
+ * @str: Pointer to the string to be printed.
+ *
+ */
+void puts2(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		if (index % 2 == 0)
+			_putchar(str[index]);
+	}
+	_putchar('\n');
+}
