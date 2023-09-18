8eb0783bfa72f3bc3082902053087350508d4114
Joseph J Bassey <136816366+Josephjbassey@users.noreply.github.com>
2023-09-18 12:08:23 -0700

    Implement the reset_to_98 function
    This function takes a pointer to an integer 'n' as a parameter and updates the value it points to by setting it to 98. It effectively resets the value to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes the pointer and updates the value
+ * @n: int to check
+ * Return: Always (0) (success)
+ */
+void reset_to_98(int *n)
+{
+	int c = 98;
+
+	*n = c;
+}
